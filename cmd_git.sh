3abeea7dc3a55994d16d3ed40f73d04c723b728d
C1549 <112643121+CactusI549@users.noreply.github.com>
2025-02-04 11:39:51 +0100

    Create cmd_git.sh

---
+++ cmd_git.sh
@@ -0,0 +1,8 @@
+# pour ajouter des modifications
+git add 
+
+# Pour faire un commit
+git commit -m "Ajout de nouvelles fonctionnalités" 
+
+# Pour pousser les modifications vers GitHub
+git push origin main 
